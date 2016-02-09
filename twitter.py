62956964f1e9175466f077ac4024af508084ee3b
zhihui <zhihui.qiu.tufts@gmail.com>
2016-02-08 21:08:09 -0500

    version 1.0
    ez code ez understand

---
+++ twitter.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+
+
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+
+#consumer key, consumer secret, access token, access secret.
+ckey="1"
+csecret="2"
+atoken="3"
+asecret="4"
+
+
+class listener(StreamListener):
+
+  def on_data(self, data):
+    print(data)
+    return True
+
+    def on_error(self,status):
+      print status
+
+auth = OAuthHandler(ckey,csecret)
+auth.set_access_token(atoken,asecret)
+
+twitterStream = Stream(auth,listener())
+twitterStream.filter(track=["google"])
